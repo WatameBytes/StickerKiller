f606f0611f17185f973c3ad3db8b357e4b0bf605
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-06-07 16:14:12 -0400

    adds a blacklist using a set

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
+// Dynamically retrieves command files
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
