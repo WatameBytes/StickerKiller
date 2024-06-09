5edd049c983837405d3342d1c579ac3bc842e8ac
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-06-09 16:17:17 -0400

    removes try-catch to reduce code repitition

--- commands/blacklist/viewbl.js
+++ commands/blacklist/viewbl.js
@@ -7,20 +7,11 @@ module.exports = {
         .setName('viewbl')
         .setDescription('View blacklisted messages'),
     async execute(interaction) {
-        try {
-            if (blacklist.size == 0) {
-                await interaction.reply('There are no blacklisted messages')
-            } else {
-                let setData = Array.from(blacklist).join('\n');
-                await interaction.reply(setData);
-            }
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({content: 'There was an error while executing this command', ephemeral: true});
-            } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
+        if (blacklist.size == 0) {
+            await interaction.reply('There are no blacklisted messages')
+        } else {
+            let setData = Array.from(blacklist).join('\n');
+            await interaction.reply('Blacklist:\n' + setData);
         }
     }
 }
