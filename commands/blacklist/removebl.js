39e95c530ec9f82d7b5959baafeef38288473860
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-06-09 21:59:20 -0400

    adds command to remove a message from the blacklist

---
+++ commands/blacklist/removebl.js
@@ -0,0 +1,17 @@
+const { blacklist } = require('./blacklist');
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('removebl')
+        .setDescription('Remove a message from the blacklist. Usage: $removebl <message_id>')
+        .addStringOption(option =>
+            option.setName('message_id')
+            .setDescription('The ID of a message you want to remove from the blacklist')
+        ),
+    async execute(interaction) {
+        const message = interaction.options.getString('message_id');
+        blacklist.delete(message);
+        await interaction.reply(`Message with id of ${message} removed from blacklist!`);
+    }
+}
