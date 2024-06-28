84f96d0c56989edc164cd679e693f53704db7a5a
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-06-28 14:19:20 -0400

    fixes small typo in clearbl

--- commands/blacklist/clearbl.js
+++ commands/blacklist/clearbl.js
@@ -5,7 +5,7 @@ const { blacklist } = require('../../utility/blacklist');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('clearbl')
-        .setDescription('VClear the blacklist'),
+        .setDescription('Clear the blacklist'),
     async execute(interaction) {
         if (blacklist.size == 0) {
             await interaction.reply('There are no blacklisted messages');
