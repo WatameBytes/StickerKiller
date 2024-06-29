ab7bc936b43988bd245d114972d22807e9e4c0ce
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-06-29 15:11:14 -0400

    edits error message

--- deploy-commands.js
+++ deploy-commands.js
@@ -27,7 +27,7 @@ for (const folder of commandFolders) {
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
+// Deploy your commands!
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
@@ -41,6 +41,6 @@ const rest = new REST().setToken(token);
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
-		console.error(error);
+		console.error(`Error loading application commands: ${error}`);
 	}
 })();
