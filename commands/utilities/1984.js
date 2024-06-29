4460bf19f08acc303dd27d75a96685c6aa149005
Jeremy Penaredondo <jay.pena1224@proton.me>
2024-06-29 15:11:00 -0400

    adds error handling and breaks loop

--- commands/utilities/1984.js
+++ commands/utilities/1984.js
@@ -18,21 +18,32 @@ module.exports = {
     async execute(interaction) {
         const user_id = interaction.options.getString('user_id');
         const max_count = interaction.options.getInteger('max_count');
-        
-        await interaction.channel.messages.fetch({ limit: 100 })
+        try {
+            interaction.channel.messages.fetch({ limit: 100 })
             .then(messages => {
                 let msgDeleted = 0;
                 for (const message of messages) {
                     // each message is an array, so message[1] returns the Message object containing author, id, and other properties
-                    if (message[1].author.id === user_id && msgDeleted < max_count && blacklist.has() === false) {
+                    if (message[1].author.id === user_id && msgDeleted < max_count && !blacklist.has(message[1].id)) {
                         // message[1] is the actual message object, not message. therefore, the delete function is called on message[1]
                         message[1].delete();
                         msgDeleted++;
                     }
+                    if (msgDeleted >= max_count) {
+                        break;
+                    }
                 }
             })
             .then(
-                await interaction.reply({content: 'Deletion complete!', ephemeral: true})
+                interaction.reply({content: 'Deletion complete!', ephemeral: true})
             );
+        } catch (error) {
+            console.error(`A(n) ${error.name} has occurred: ${error.message}.`);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({content: error.message, ephemeral: true});
+            } else {
+                await interaction.reply({ content: error.message, ephemeral: true });
+            }
+        }
     },
 }
